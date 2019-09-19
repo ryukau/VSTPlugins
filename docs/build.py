ca745debacdcfe3607548c23419e02420664a017
Takamitsu Endo <ryukau@gmail.com>
2019-09-19 21:28:59 +0900

    Add pandoc build for documentation

--- docs/build.py
+++ docs/build.py
@@ -1,1 +1,19 @@
-pandoc -s --css=../../css/style.css  -o SevenDelay_ja.html .\SevenDelay_ja.md
+import subprocess
+from pathlib import Path
+
+css_path = Path("style.css").resolve()
+template_path = Path("template.html").resolve()
+for md in Path(".").glob("**/*.md"):
+    subprocess.run([
+        "pandoc",
+        "-s",
+        "--toc",
+        "--toc-depth=4",
+        "--metadata",
+        f"title={md.stem}",
+        f"--template={str(template_path.resolve().as_uri())}",
+        f"--css={str(css_path.resolve().as_uri())}",
+        "-o",
+        f"{md.with_suffix('')}.html",
+        str(md),
+    ])
