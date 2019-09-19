d8e305373b9b2e713fcd51182dbcb0dc5fa17b3e
Takamitsu Endo <ryukau@gmail.com>
2019-09-19 21:57:20 +0900

    Fix css path to relative path

--- docs/build.py
+++ docs/build.py
@@ -1,9 +1,11 @@
 import subprocess
+import os
 from pathlib import Path
 
-css_path = Path("style.css").resolve()
-template_path = Path("template.html").resolve()
+css_path = Path("style.css")
+template_path = str(Path("template.html").resolve().as_uri())
 for md in Path(".").glob("**/*.md"):
+    relative_css_path = os.path.relpath(css_path, md.parent).replace('\\', '/')
     subprocess.run([
         "pandoc",
         "-s",
@@ -11,8 +13,8 @@ for md in Path(".").glob("**/*.md"):
         "--toc-depth=4",
         "--metadata",
         f"title={md.stem}",
-        f"--template={str(template_path.resolve().as_uri())}",
-        f"--css={str(css_path.resolve().as_uri())}",
+        f"--template={template_path}",
+        f"--css={relative_css_path}",
         "-o",
         f"{md.with_suffix('')}.html",
         str(md),
