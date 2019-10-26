4976da490c369113fdbd89425806f89bb824994c
cu <cu@localhost.localdomain>
2019-10-26 10:21:11 +0900

    Add plugin source path

--- ci/ci_ubuntu_latest.sh
+++ ci/ci_ubuntu_latest.sh
@@ -14,7 +14,11 @@ git submodule update --init --recursive
 
 mkdir build
 cd build || exit
-cmake -DCMAKE_BUILD_TYPE=Release -DSMTG_ADD_VST3_HOSTING_SAMPLES=FALSE -DSMTG_ADD_VST3_PLUGINS_SAMPLES=FALSE ..
+cmake -DCMAKE_BUILD_TYPE=Release \
+  -DSMTG_MYPLUGINS_SRC_PATH="$GITHUB_WORKSPACE" \
+  -DSMTG_ADD_VST3_HOSTING_SAMPLES=FALSE \
+  -DSMTG_ADD_VST3_PLUGINS_SAMPLES=FALSE \
+  ..
 cmake --build .
 
 # No idea how to set $HOME as path of actions/upload-artifact@v1.
