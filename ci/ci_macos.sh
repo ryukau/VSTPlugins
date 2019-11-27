4d0a049674f6ba27903e6aa789a2615a74e8786c
cu <cu@localhost.localdomain>
2019-11-27 09:25:42 +0900

    Add -GXcode option

--- ci/ci_macos.sh
+++ ci/ci_macos.sh
@@ -11,12 +11,12 @@ cd vst3sdk || exit
 
 mkdir build
 cd build || exit
-cmake -DCMAKE_BUILD_TYPE=Release \
+cmake -GXcode \
   -DSMTG_MYPLUGINS_SRC_PATH="$GITHUB_WORKSPACE" \
   -DSMTG_ADD_VST3_HOSTING_SAMPLES=FALSE \
   -DSMTG_ADD_VST3_PLUGINS_SAMPLES=FALSE \
   ..
-cmake --build .
+cmake --build . --config Release
 
 # No idea how to set $HOME as path of actions/upload-artifact@v1.
 mv "$HOME"/vst3sdk/build/VST3/Release "$GITHUB_WORKSPACE"/vst_macOS
