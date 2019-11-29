50305a742c923f6407e3c53d059f721fa9750299
cu <cu@localhost.localdomain>
2019-11-29 13:08:22 +0900

    Add header and footer for documentation
    - Last modified
    - Back to index link
    - Table of contents

--- docs/build.py
+++ docs/build.py
@@ -1,7 +1,22 @@
 import subprocess
 import os
+import time
 from pathlib import Path
 
+def get_last_modified(md):
+    result = subprocess.run(
+        ["git", "log", "-1", '--date=format:%F', '--format="%cd"', "--", md],
+        stdout=subprocess.PIPE,
+        encoding="utf-8",
+    )
+    if len(result.stdout) > 0:
+        return result.stdout.rstrip()[1:-1]  # remove double quotation (").
+
+    # if there are no commits yet, get it from system.
+    epoch = Path(md).stat().st_mtime
+    return time.strftime("%F", time.localtime(epoch))
+
+index_path = Path("index.html").resolve()
 css_path = Path("style.css").resolve()
 template_path = Path("template.html").resolve()
 if os == "nt":
@@ -9,6 +24,12 @@ if os == "nt":
     template_path = template_path.as_uri()
 
 for md in Path(".").glob("**/*.md"):
+    index_relpath = os.path.relpath(str(index_path), str(md.parent.resolve()))
+    if index_relpath == "index.html":
+        index_relpath = ""
+
+    last_modified = get_last_modified(md)
+
     subprocess.run([
         "pandoc",
         "-s",
@@ -16,6 +37,10 @@ for md in Path(".").glob("**/*.md"):
         "--toc-depth=4",
         "--metadata",
         f"title={md.stem}",
+        "--metadata",
+        f"date-meta={last_modified}",
+        "--metadata",
+        f"index-relative-path={index_relpath}",
         f"--template={str(template_path)}",
         f"-H",
         css_path,
