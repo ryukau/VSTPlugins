4e9cea9fe7b94d94f3eccd3ff6891f8e4592f658
Takamitsu Endo <ryukau@gmail.com>
2021-03-12 22:38:03 +0900

    Add preset jsons

---
+++ presets/extractparameter.py
@@ -0,0 +1,140 @@
+import re
+import json
+import sys
+from pathlib import Path
+
+def convert_string(string: str):
+    try:
+        value = int(string)
+        return value
+    except ValueError:
+        try:
+            value = float(string)
+            return value
+        except ValueError:
+            if string == "true":
+                return True
+            elif string == "false":
+                return False
+            return string
+
+def get_enum_range(code):
+    # Sort parameter by the order defined in enum ParameterID::ID.
+    found = re.findall(
+        r"namespace ParameterID \{\nenum ID.*\{([\s\S]*)\};\n\} // namespace ParameterID",
+        code)
+
+    enum = []
+    for line in found[0].split("\n"):
+        line = line.strip()
+        if line == "":
+            continue
+        if line[0] == "/":
+            continue
+        line = re.split(r"//", line)[0]
+        enum.append(line.replace(",", ""))
+
+    temp = []
+    index = -1
+    for line in enum:
+        if line.find("=") == -1:
+            index += 1
+            temp.append({"name": line, "index": index})
+            continue
+        name, end_str = line.replace(" ", "").split("=")
+        index = int(end_str)
+        temp.append({"name": name, "index": index})
+
+    index = 0
+    for idx in range(len(enum) - 1):
+        temp[idx]["range"] = temp[idx + 1]["index"] - temp[idx]["index"]
+    temp.pop() # Delete ID_ENUM_LENGTH
+
+    enum = {}
+    for elem in temp:
+        name = elem.pop("name")
+        enum[name] = elem
+    return enum
+
+def extract_plugin_parameter(parameter_hpp_path):
+    with open(parameter_hpp_path, "r") as fi:
+        code = fi.read()
+
+    plugin_name = re.findall(r"This file is part of (.*)\n", code)[0][:-1]
+    enum = get_enum_range(code)
+
+    # Using python struct module format character to represent type.
+    # "I" is unsigned int with standard size of 4 bytes.
+    # "d" is double with standard size of 8 bytes.
+    index = 0
+    data = []
+    for matched in re.findall(
+            r"value\[.*ID::(.*)\]\s*=\s*std::make_unique<(.*)>\(\s*([^,]*),\s*([^,]*),\s*[^,]*,\s*([^\)]*)\);",
+            code,
+    ):
+        datatype = None
+        if matched[1] == "UIntValue":
+            datatype = "I"
+        else:
+            datatype = "d"
+
+        if matched[0].find(" + i") != -1:                 # TODO: Change this to regex.
+            name, _, _ = matched[0].rpartition(" + i")
+            basename = name[0:-1]                         # remove '0'.
+            for i in range(enum[name]["range"]):
+                default = matched[2].replace("idx", str(i))
+                data.append({
+                    "id": index,
+                    "name": f"{basename}{i}",
+                    "type": datatype,
+                    "default": convert_string(default),
+                    "scale": matched[3],
+                    "flags": matched[4],
+                })
+                index += 1
+        else:
+            data.append({
+                "id": index,
+                "name": matched[0],
+                "type": datatype,
+                "default": convert_string(matched[2]),
+                "scale": matched[3],
+                "flags": matched[4],
+            })
+            index += 1
+
+    json_dir = Path(__file__).parent / Path("json")
+    json_dir.mkdir(parents=True, exist_ok=True)
+    with open(json_dir / Path(f"{plugin_name}.type.json"), "w") as fi:
+        json.dump(data, fi, indent=2)
+
+def extract_parameter():
+    for path in Path("..").glob("**/parameter.hpp"):
+        if path.parts[1] == "_dump":
+            continue
+        print(path)
+        extract_plugin_parameter(path)
+
+def extract_guid():
+    re_processor_guid = re.compile(
+        r"static const FUID ProcessorUID\(0x(.*), 0x(.*), 0x(.*), 0x(.*)\);")
+
+    data = {}
+    for path in Path("..").glob("**/fuid.hpp"):
+        if path.parts[1] == "_dump":
+            continue
+
+        with open(path, "r", encoding="utf-8") as fi:
+            code = fi.read()
+
+        found = re_processor_guid.findall(code)
+        data[path.parts[-3]] = {
+            "processor_guid": "".join(found[0]),
+        }
+
+    with open("json/GUID.json", "w", encoding="utf-8") as fi:
+        json.dump(data, fi)
+
+if __name__ == "__main__":
+    extract_guid()
+    extract_parameter()
---
+++ presets/vstpresettojson.py
@@ -0,0 +1,159 @@
+"""
+This script converts *.vstpreset to json. Requires parameter data structure defined in plugin.
+
+VST 3 Preset file specification:
+https://steinbergmedia.github.io/vst3_doc/vstinterfaces/vst3loc.html#presetformat
+
+Note: offset is number of bytes from start of vstpreset file. Arrows in the figure at above link are misleading.
+"""
+
+import json
+import struct
+import sys
+
+from pathlib import Path
+
+def print_data(data):
+    """
+    data = {
+        "chunk_info" : { ... },
+        "entry0" : data0,
+        "entry1" : data1,
+        ...
+    }
+    """
+    for key, chunk in data.items():
+        print(f"-- {key}")
+        if key == "chunk_info":
+            for name, value in chunk.items():
+                print(f"{name:<24} {value}")
+        else:
+            print(chunk)
+        print()
+
+def read_vstpreset(path, endian):
+    """
+    Return value for vstpreset version 1.
+
+    data = {
+        "chunk_info" : { ... },
+        "Comp" : Comp_data_in_byte_sequence,
+        "Cont" : Cont_data_in_byte_sequence,
+        "Info" : Info_xml_in_utf-8_text,
+    }
+    """
+    data = {}
+    with open(path, "rb") as fi:
+        info = {}
+        info["header_id"] = fi.read(4).decode("ascii")
+        if info["header_id"] != "VST3":
+            raise RuntimeError(f"{path} is not vstpreset.")
+        info["version"] = int.from_bytes(fi.read(4), endian)
+        info["class_id"] = fi.read(32).decode("ascii")
+        info["chunk_list_offset"] = int.from_bytes(fi.read(8), endian)
+
+        # Skip to CHUNK LIST section.
+        fi.seek(info["chunk_list_offset"], 0)
+
+        info["list_id"] = fi.read(4).decode("ascii")
+        info["entry_count"] = int.from_bytes(fi.read(4), endian)
+
+        entry = []
+        for n in range(info["entry_count"]):
+            section = fi.read(4).decode("ascii")
+            offset = int.from_bytes(fi.read(8), endian)
+            size = int.from_bytes(fi.read(8), endian)
+
+            entry.append({
+                "id": section,
+                "offset": offset,
+                "size": size,
+            })
+
+            info[f"{section}_id"] = section
+            info[f"{section}_offset"] = offset
+            info[f"{section}_size"] = size
+
+        data["chunk_info"] = info
+
+        for ent in entry:
+            if ent["id"] == "Comp":
+                fi.seek(ent["offset"], 0)
+                data[ent["id"]] = fi.read(ent["size"])
+            elif ent["id"] == "Info":
+                fi.seek(ent["offset"], 0)
+                data[ent["id"]] = fi.read(ent["size"]).decode("utf-8")
+            else:
+                fi.seek(ent["offset"], 0)
+                data[ent["id"]] = fi.read(ent["size"])
+    return data
+
+def vstpreset_to_json(preset_plugin_dir,
+                      endian="little",
+                      dump_json=True,
+                      debug_print_data=False):
+    """
+    preset_list["data"] is optional debug info.
+
+    preset_list = [
+        { "name": name, "parameter", {"param_name": value, ...} },
+        ...
+    ]
+    """
+    print(f"Processing: {preset_plugin_dir}")
+    preset_plugin_dir = Path(preset_plugin_dir)
+    plugin_name = preset_plugin_dir.stem
+    json_dir = Path(__file__).parent / Path("json")
+
+    preset_list = []
+    for vstpreset_path in sorted(preset_plugin_dir.glob("**/*.vstpreset")):
+        data = read_vstpreset(vstpreset_path, endian)
+        if debug_print_data: # For debug.
+            print(f"-- filename\n{vstpreset_path}\n")
+            print_data(data)
+
+        # type_data = [{"id":, "name":, "type":, "default":, "scale":, "flags":,}, ... ]
+        with open(json_dir / Path(f"{plugin_name}.type.json"), "r") as fi:
+            type_data = json.load(fi)
+
+        comp_data = data["Comp"]
+        idx = 0
+        param = []
+
+        for info in type_data:
+            # TODO: delete this branch after fixing LPS presets.
+            if preset_plugin_dir.stem == "LightPadSynth" and (info["id"] == 1506 or
+                                                              info["id"] == 1513):
+                param.append({
+                    "name": info["name"],
+                    "type": info["type"],
+                    "value": info["default"],
+                })
+                continue
+
+            type_char = info["type"]
+            n_byte = struct.calcsize(type_char)
+            value = struct.unpack(type_char, comp_data[idx:idx + n_byte])[0]
+            idx += n_byte
+            param.append({
+                "name": info["name"],
+                "type": info["type"],
+                "value": value,
+            })
+
+        preset = {}
+        preset["name"] = vstpreset_path.stem
+        preset["parameter"] = param
+        preset_list.append(preset)
+
+    if dump_json:
+        with open(json_dir / Path(f"{plugin_name}.preset.json"), "w") as fi:
+            json.dump(preset_list, fi, indent=2)
+    return preset_list
+
+if __name__ == "__main__":
+    for path in Path("Uhhyou").glob("*"):
+        if not path.is_dir():
+            continue
+        vstpreset_to_json(path, "little")
+    # vstpreset_to_json(Path("Uhhyou/LightPadSynth"), "little", debug_print_data=True)
