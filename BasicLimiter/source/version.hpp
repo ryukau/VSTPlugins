850d0b5b5aad4a13d7a8c357d083e46b40a40c43
Takamitsu Endo <ryukau@gmail.com>
2022-04-24 20:03:40 +0900

    Update version

--- BasicLimiter/source/version.hpp
+++ BasicLimiter/source/version.hpp
@@ -26,14 +26,14 @@
 #define MAJOR_VERSION_STR "0"
 #define MAJOR_VERSION_INT 0
 
-#define SUB_VERSION_STR "0"
-#define SUB_VERSION_INT 0
+#define SUB_VERSION_STR "1"
+#define SUB_VERSION_INT 1
 
-#define RELEASE_NUMBER_STR "1"
-#define RELEASE_NUMBER_INT 1
+#define RELEASE_NUMBER_STR "0"
+#define RELEASE_NUMBER_INT 0
 
-#define BUILD_NUMBER_STR "1"
-#define BUILD_NUMBER_INT 1
+#define BUILD_NUMBER_STR "2"
+#define BUILD_NUMBER_INT 2
 
 #define FULL_VERSION_STR                                                                 \
   MAJOR_VERSION_STR "." SUB_VERSION_STR "." RELEASE_NUMBER_STR "." BUILD_NUMBER_STR
