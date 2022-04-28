9b3489bfa622b23def4555a3de3675409c66b978
Takamitsu Endo <ryukau@gmail.com>
2022-04-29 02:08:11 +0900

    Update version

--- CollidingCombSynth/source/version.hpp
+++ CollidingCombSynth/source/version.hpp
@@ -29,11 +29,11 @@
 #define SUB_VERSION_STR "1"
 #define SUB_VERSION_INT 1
 
-#define RELEASE_NUMBER_STR "1"
-#define RELEASE_NUMBER_INT 1
+#define RELEASE_NUMBER_STR "2"
+#define RELEASE_NUMBER_INT 2
 
-#define BUILD_NUMBER_STR "1"
-#define BUILD_NUMBER_INT 1
+#define BUILD_NUMBER_STR "2"
+#define BUILD_NUMBER_INT 2
 
 #define FULL_VERSION_STR                                                                 \
   MAJOR_VERSION_STR "." SUB_VERSION_STR "." RELEASE_NUMBER_STR "." BUILD_NUMBER_STR
