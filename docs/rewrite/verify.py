3569fe6e0c0561df727a471ff37f024d13032f20
Takamitsu Endo <ryukau@gmail.com>
2022-05-24 23:58:28 +0900

    Change verify.py to handle no connection error
    Also changed to write results to `verify.txt`.

--- docs/rewrite/verify.py
+++ docs/rewrite/verify.py
@@ -7,7 +7,10 @@ session = requests.Session()
 adapter = requests.adapters.HTTPAdapter(pool_connections=100, pool_maxsize=2048)
 session.mount("", adapter)
 
+log_text = ""
 for path in Path("..").glob("**/*.html"):
+    if path.stem == "vst3_dev":
+        continue
     with open(path, "r") as fi:
         raw_html = fi.read()
     soup = BeautifulSoup(raw_html, "lxml")
@@ -17,14 +20,25 @@ for path in Path("..").glob("**/*.html"):
         parsed = urllib.parse.urlparse(link)
         if len(parsed.scheme) != 0:
             url = urllib.parse.urlunparse(parsed)
-            req = session.head(url)
 
-            log = f"{path} {url} {req.status_code}"
+            try:
+                req = session.head(url)
+            except requests.exceptions.ConnectionError:
+                text = f"No Connection " + url
+                print(text)
+                log_text += text + "\n"
+                continue
+
+            status_text = f"{path} {url} {req.status_code}"
 
             status_class = req.status_code // 100
             if status_class == 4 or status_class == 5:
-                log = "Error   " + log
+                status_text = "Error   " + status_text
             else:
-                log = "Success " + log
+                status_text = "Success " + status_text
 
-            print(log)
+            print(status_text)
+            log_text += status_text + "\n"
+
+with open("verify.txt", "w", encoding="utf-8") as fi:
+    fi.write(log_text)
