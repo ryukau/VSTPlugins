04fefe761af8fc07cda93ade0eff1744bde6b9a5
Takamitsu Endo <ryukau@gmail.com>
2022-06-04 09:05:14 +0900

    Update version

--- FDNCymbal/source/version.hpp
+++ FDNCymbal/source/version.hpp
@@ -29,11 +29,11 @@
 #define SUB_VERSION_STR "2"
 #define SUB_VERSION_INT 2
 
-#define RELEASE_NUMBER_STR "10"
-#define RELEASE_NUMBER_INT 10
+#define RELEASE_NUMBER_STR "11"
+#define RELEASE_NUMBER_INT 11
 
-#define BUILD_NUMBER_STR "17"
-#define BUILD_NUMBER_INT 17
+#define BUILD_NUMBER_STR "18"
+#define BUILD_NUMBER_INT 18
 
 #define FULL_VERSION_STR                                                                 \
   MAJOR_VERSION_STR "." SUB_VERSION_STR "." RELEASE_NUMBER_STR "." BUILD_NUMBER_STR
