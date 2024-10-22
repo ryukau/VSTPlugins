b210ae35524449827e9f0a1f549b1f06f969c504
Takamitsu Endo <ryukau@gmail.com>
2024-10-23 07:09:06 +0900

    Change to create all-in-one zip package

--- package/pack.py
+++ package/pack.py
@@ -19,19 +19,18 @@ def check_file(file_path):
         exit(1)
 
 
-def create_full_archive_from_github_actions_artifact(version, manual_root_dir: Path):
+def create_archive_from_github_actions_artifact(version, pack_dir: Path, isMacOS: bool):
     with open("manual.json", "r", encoding="utf-8") as fi:
         manual_dict = json.load(fi)
 
-    pack_dir = Path("pack")
     pack_dir.mkdir(exist_ok=True)
 
     # For manual packaging.
-    for zip_file in Path().glob("*.zip"):
+    for zip_file in Path().glob("vst_macOS.zip" if isMacOS else "vst_*.zip"):
         shutil.unpack_archive(zip_file, pack_dir, format="zip")
 
     # For GitHub Actions.
-    for artifact_dir in Path().glob("vst_*"):
+    for artifact_dir in Path().glob("vst_macOS" if isMacOS else "vst_*"):
         if not artifact_dir.is_dir():
             continue
         shutil.copytree(artifact_dir, pack_dir, dirs_exist_ok=True)
@@ -43,6 +42,7 @@ def create_full_archive_from_github_actions_artifact(version, manual_root_dir: P
     for plugin_ico in pack_dir.glob("**/PlugIn.ico"):
         plugin_ico.unlink(plugin_ico)
 
+    archive_postfix = "_macOS" if isMacOS else ""
     for vst3_dir in pack_dir.glob("*.vst3"):
         plugin_name = vst3_dir.stem
 
@@ -51,76 +51,60 @@ def create_full_archive_from_github_actions_artifact(version, manual_root_dir: P
         )
 
         # Do not make package if a plugin doesn't have manual.
-        plugin_manual_dir = manual_root_dir / Path(f"{manual_name}")
+        plugin_manual_dir = Path("../docs/manual/") / Path(f"{manual_name}")
         if not plugin_manual_dir.exists():
             print(f"Skipping {plugin_name}: manual not found")
             continue
 
-        copy_resource(
-            plugin_manual_dir,
-            vst3_dir / Path("Contents/Resources/Documentation"),
-            f"{plugin_name} manual was not found",
-        )
+        if isMacOS:
+            # No manual to not mess up codesign.
+            check_file(vst3_dir / Path(f"Contents/MacOS/{plugin_name}"))
+        else:
+            copy_resource(
+                plugin_manual_dir,
+                vst3_dir / Path("Contents/Resources/Documentation"),
+                f"{plugin_name} manual was not found",
+            )
 
-        # Ensuring that no binary is missing.
-        check_file(vst3_dir / Path(f"Contents/x86_64-win/{plugin_name}.vst3"))
-        check_file(vst3_dir / Path(f"Contents/x86_64-linux/{plugin_name}.so"))
-        check_file(vst3_dir / Path(f"Contents/MacOS/{plugin_name}"))
+            check_file(vst3_dir / Path(f"Contents/x86_64-win/{plugin_name}.vst3"))
+            check_file(vst3_dir / Path(f"Contents/x86_64-linux/{plugin_name}.so"))
+            check_file(vst3_dir / Path(f"Contents/MacOS/{plugin_name}"))
 
         create_zip_archive(
-            vst3_dir.parent / Path(f"{plugin_name}_{version[plugin_name]}"),
+            vst3_dir.parent
+            / Path(f"{plugin_name}_{version[plugin_name]}{archive_postfix}"),
             plugin_name,
             vst3_dir,
             Path(f"../presets/Uhhyou/{plugin_name}"),
         )
 
+    # Create all-in-one archive.
+    project_version = version["UhhyouPlugins"]
+    all_dir = pack_dir / Path(f"UhhyouPlugins_{project_version}{archive_postfix}")
+    all_dir.mkdir(parents=True, exist_ok=True)
 
-def create_macos_archive_from_github_actions_artifact(version, manual_root_dir: Path):
-    """
-    Ad-hoc method. Merge this to `create_full_archive_from_github_actions_artifact`
-    in future. Also remove `pack_macOS` line from `.github/workflows/build.yml`.
-    """
-    with open("manual.json", "r", encoding="utf-8") as fi:
-        manual_dict = json.load(fi)
+    all_plugin_dir = all_dir / Path("plugins")
+    all_plugin_dir.mkdir(parents=True, exist_ok=True)
 
-    pack_dir = Path("pack_macOS")
-    pack_dir.mkdir(exist_ok=True)
+    # Without `list()`, it recursively globs into `all_plugin_dir`.
+    globbed = list(pack_dir.glob("**/*.vst3"))
 
-    # For manual packaging.
-    for zip_file in Path().glob("vst_macOS.zip"):
-        shutil.unpack_archive(zip_file, pack_dir, format="zip")
-
-    # For GitHub Actions.
-    for artifact_dir in Path().glob("vst_macOS"):
-        if not artifact_dir.is_dir():
+    for plugin_dir in globbed:
+        if not plugin_dir.is_dir():
             continue
-        shutil.copytree(artifact_dir, pack_dir, dirs_exist_ok=True)
-
-    for dsym in pack_dir.glob("*.dSYM"):
-        shutil.rmtree(dsym)
-
-    for vst3_dir in pack_dir.glob("*.vst3"):
-        plugin_name = vst3_dir.stem
-
-        manual_name = (
-            plugin_name if not plugin_name in manual_dict else manual_dict[plugin_name]
-        )
-
-        # Do not make package if a plugin doesn't have manual.
-        plugin_manual_dir = manual_root_dir / Path(f"{manual_name}")
-        if not plugin_manual_dir.exists():
-            print(f"Skipping {plugin_name}: manual not found")
+        if plugin_dir.parent == pack_dir:
             continue
+        shutil.move(plugin_dir, all_plugin_dir)
 
-        # Ensuring that no binary is missing.
-        check_file(vst3_dir / Path(f"Contents/MacOS/{plugin_name}"))
+    all_preset_dir = all_dir / Path("presets")
+    all_preset_dir.mkdir(parents=True, exist_ok=True)
 
-        create_zip_archive(
-            vst3_dir.parent / Path(f"{plugin_name}_{version[plugin_name]}_macOS"),
-            plugin_name,
-            vst3_dir,
-            Path(f"../presets/Uhhyou/{plugin_name}"),
-        )
+    src_preset_dir = Path("../presets/Uhhyou")
+    shutil.copytree(
+        src_preset_dir, all_preset_dir / src_preset_dir.parts[-1], dirs_exist_ok=True
+    )
+
+    shutil.make_archive(all_dir, "zip", all_dir)
 
 
 def create_zip_archive(archive_dir, plugin_name, vst3_dir, presets_dir):
@@ -159,6 +143,13 @@ def get_version():
     plugins = re.findall(r"add_subdirectory\(([A-Z].+?)\)", top_level_cmake_text)
 
     version = {}
+
+    version["UhhyouPlugins"] = re.search(
+        r"project\(UhhyouPlugins\s+VERSION\s+([0-9.]+)\)",
+        top_level_cmake_text,
+        flags=re.MULTILINE | re.DOTALL,
+    ).groups()[0]
+
     for plugin_name in plugins:
         version_hpp = Path("../") / Path(plugin_name) / Path("source/version.hpp")
         if not version_hpp.exists():
@@ -178,6 +169,5 @@ def get_version():
 
 if __name__ == "__main__":
     version = get_version()
-    manual_root_dir = "../docs/manual/"
-    create_macos_archive_from_github_actions_artifact(version, manual_root_dir)
-    create_full_archive_from_github_actions_artifact(version, manual_root_dir)
+    create_archive_from_github_actions_artifact(version, Path("pack"), False)
+    create_archive_from_github_actions_artifact(version, Path("pack_macOS"), True)
